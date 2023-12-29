60168fda237332c4909965d26d49c1327d788c31
ateethBhattarai <ateeth.bhattarai02@gmail.com>
2023-12-29 09:03:42 +0545

    edit blog backend

--- backend/Controllers/blogController.js
+++ backend/Controllers/blogController.js
@@ -13,12 +13,13 @@ const createBlog = async (req, res) => {
 const individualBlog = async (req, res) => {
   const { id } = req.params;
   try {
-    const blogData = await blogModel.findOne({ _id: id }).populate("author");
+    const blogData = await blogModel
+      .findOne({ _id: id })
+      .populate({ path: "author", select: "-password" });
     if (!blogData) {
       res.status(404).send("blog not found.");
       return;
     }
-    // console.log(blogData);
     res.json(blogData);
   } catch (error) {
     res.status(404).send("blog not found.");
@@ -26,12 +27,40 @@ const individualBlog = async (req, res) => {
 };
 
 const allBlog = async (req, res) => {
-  const blogData = await blogModel.find().sort({ createdAt: -1 });
+  const blogData = await blogModel
+    .find()
+    .sort("-createdAt")
+    .populate({ path: "author", select: "-password" });
+
   if (!blogData) {
     res.status(404).send("No blog created yet.");
     return;
   }
+
   res.json(blogData);
+};
+
+const editBlog = async (req, res) => {
+  const { id } = req.params;
+  const { body } = req;
+
+  try {
+    const haveBlog = await blogModel.findById(id);
+    if (!haveBlog) {
+      res.json({ message: "No blog Found!!" });
+      return;
+    }
+
+    const updatedBlog = await blogModel.findOneAndUpdate(
+      { _id: id },
+      { $set: body },
+      { new: true }
+    );
+
+    res.json(updatedBlog);
+  } catch (error) {
+    res.json({ error });
+  }
 };
 
 const deleteBlog = async (req, res) => {
@@ -48,4 +77,4 @@ const deleteBlog = async (req, res) => {
   }
 };
 
-module.exports = { createBlog, individualBlog, allBlog, deleteBlog };
+module.exports = { createBlog, individualBlog, allBlog, editBlog, deleteBlog };
--- backend/Routers/blogRouter.js
+++ backend/Routers/blogRouter.js
@@ -4,12 +4,14 @@ const {
   individualBlog,
   allBlog,
   deleteBlog,
+  editBlog,
 } = require("../Controllers/blogController");
 const router = express.Router();
 
 router.post("/create", createBlog);
 router.get("/allBlog", allBlog);
 router.get("/:id", individualBlog);
+router.put("/edit/:id", editBlog);
 router.delete("/:id", deleteBlog);
 
 module.exports = router;
